d52dca59937a29db6369ec9a23c94693b3e1a2f6
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-31 12:15:09 +0100

    add example

--- example/index.js
+++ example/index.js
@@ -1,5 +1,7 @@
 var tabs = TABS = require('../')
 var autonode = require('autonode').inject(tabs)
+var Repred   = require('repred')
+var h        = require('h')
 
 function log(name, stream) {
   stream.on('data', function (data) {
@@ -8,25 +10,34 @@ function log(name, stream) {
   return stream
 }
 
+//just update a singe value...
+var repred = Repred(function (value) {
+    return {ts: Date.now(), val: value}
+  }, function (cur, up) {
+    if(cur.ts < up.ts || !cur.ts) {
+      cur.ts = up.ts
+      cur.val = up.val
+      return up
+    }
+  },
+  {})
+
 console.log(tabs)
 
 autonode(function (stream) {
   console.log('autonode - connect')
+  stream.pipe(repred.createStream()).pipe(stream)
 }).listen('hi')
 
-/*
-tabs.createServer(function (stream) {
-  console.log('server - connection')
-  log('server', stream)
-  //THIS DOESN'T WORK, BECAUSE IS WRITING BACK THE SAME VALUE,
-  //WHICH DOESN'T EMIT AN EVENT. guess the solution is to write 
-  //a count prefix so that the value always changes.
-  stream.pipe(stream)
-}).on('error', function () {
-  console.log('client - connection')
-  CLIENT = log('client', tabs.connect('hello'))
-  setInterval(function () {
-    CLIENT.write(new Date())
-  }, 1e3)
-}).listen('hello')
-*/
+var input = h('input', {input: function () {
+    repred.update(input.value)
+  }
+})
+
+repred.on('update', function (up) {
+//  if(input.value != up.val)
+    input.value = up.val
+})
+
+document.body.appendChild(input)
+
