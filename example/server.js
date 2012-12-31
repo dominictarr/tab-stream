bc6cfbd4708ef6613c6b2bd9b5292d00422068e1
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-31 13:23:11 +0100

    note on the server in example

--- example/server.js
+++ example/server.js
@@ -1,5 +1,6 @@
-
-
+//we need a server even though the example is static,
+//because just running the app as a file does not share a domain,
+//so the tabs do not get shared access to localStorage.
 var http = require('http')
 var Ecstatic = require('ecstatic')
 http.createServer(
